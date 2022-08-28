eee622daba0bf6845b665a6b48ad4b9f7bfa8d88
Abraham Hidy <abraham.hidy@neava.se>
2022-08-28 21:07:07 +0200

    add cloud session expander widget

--- lib/controllers/userController.dart
+++ lib/controllers/userController.dart
@@ -1,1 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
+
+class UserController {
+  UserController._privateConstructor() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
+
+  static final UserController _instance = UserController._privateConstructor();
+
+  factory UserController() {
+    return _instance;
+  }
+}
---
+++ lib/widgets/cloudSessionsExpander.dart
@@ -0,0 +1,72 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:cloud_calc/models/calculation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class CloudSessionsExpander extends StatefulWidget {
+  const CloudSessionsExpander({Key? key}) : super(key: key);
+
+  @override
+  State<CloudSessionsExpander> createState() => _CloudSessionsExpanderState();
+}
+
+class _CloudSessionsExpanderState extends State<CloudSessionsExpander> {
+  bool _open = false;
+
+  void _expandToggle(bool? open) {
+    setState(() {
+      _open = open ?? !_open;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: _open ? MediaQuery.of(context).size.height - 30 : 40,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.elliptical(10, 10),
+          bottomRight: Radius.elliptical(10, 10),
+        ),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black54,
+              blurRadius: 15.0,
+              offset: Offset(0.0, 0.65))
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Expanded(
+            child: Column(
+              children: [],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () => _expandToggle(null),
+                icon: Icon(_open ? Icons.arrow_drop_up : Icons.arrow_drop_down),
+              ),
+              Text(
+                "Unsaved",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+              IconButton(
+                onPressed: null,
+                icon: Icon(Icons.person),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
