7dac1bcb5ac56268e5b43615768216f723ba9136
Abraham Hidy <abraham.hidy@neava.se>
2022-08-29 23:46:03 +0200

    create account widget

--- lib/widgets/cloudSessionsExpander.dart
+++ lib/widgets/cloudSessionsExpander.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cloud_calc/models/calculation.dart';
+import 'package:cloud_calc/widgets/account.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -12,8 +13,6 @@ class CloudSessionsExpander extends StatefulWidget {
 
 class _CloudSessionsExpanderState extends State<CloudSessionsExpander> {
   bool _open = false;
-
-  final _formKey = GlobalKey<FormState>();
 
   void _expandToggle(bool? open) {
     setState(() {
@@ -41,67 +40,7 @@ class _CloudSessionsExpanderState extends State<CloudSessionsExpander> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Login"),
-                  const SizedBox(height: 30),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Your email",
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 15),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Choose a password",
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: const Color.fromARGB(255, 3, 46, 132)),
-                    onPressed: () {
-                      // Validate returns true if the form is valid, or false otherwise.
-                      if (_formKey.currentState!.validate()) {
-                        // If the form is valid, display a snackbar. In the real world,
-                        // you'd often call a server or save the information in a database.
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Processing Data')),
-                        );
-                      }
-                    },
-                    child: const Text('Submit'),
-                  ),
-                  const SizedBox(height: 60),
-                  Row(
-                    children: [
-                      Text("Need an account?"),
-                      TextButton(
-                        onPressed: null,
-                        child: Text("Get a login"),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
+          Account(),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
